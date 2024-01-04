2fc7e9fa86d8e03f943dd90ab6c7973aa1aff8d7
fauzanlnh <fauzanlnh@gmail.com>
2024-01-04 20:18:53 +0700

    Create files in controller, models, and database folders

---
+++ database/seeders/ReturnPaymentBookSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReturnPaymentBookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
